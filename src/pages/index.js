e62a516ccab574713e1e4cb6f6e07c4259160ebd
Paweł Karniej <karniej.p@gmail.com>
2020-08-07 12:10:17 +0200

    More fixes in the design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 import PropTypes from 'prop-types'
 import { graphql, Link } from 'gatsby'
 import { kebabCase } from 'lodash'
@@ -6,71 +7,70 @@ import Layout from '../components/Layout'
 import Form from '../components/Form'
 import Sidebar from '../components/Sidebar'
 
-export default class IndexPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+const IndexPage = ({ data }) => {
+  const { edges: posts } = data.allMarkdownRemark
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
 
-    return (
-      <Layout>
-        <section className="section">
-          <div className="container content content-container">
-            <Form />
-            <div className="columns">
-              <div className="column">
-                {posts.map(({ node: post }) => {
-                  return (
-                    <div key={post.id} className="post">
-                      <a href={post.fields.slug}>
-                        <h1 className="post-title">{post.frontmatter.title}</h1>
-                      </a>
-                      <a href={post.fields.slug}>
-                        <div className="post-inner">
-                          <img
-                            className="post-thumbnail"
-                            alt="post thumbnail"
-                            src={
-                              post.frontmatter.thumbnail.childImageSharp.fluid
-                                .src
-                            }
-                          />
-                          <p className="post-excerpt">{post.excerpt}</p>
-                        </div>
-                      </a>
-                      <div className="post-footer">
-                        {post.frontmatter.tags && (
-                          <ul className="post-tags">
-                            {post.frontmatter.tags.map((tag) => (
-                              <li key={tag + `tag`}>
-                                <span className="tag is-primary post-tag">
-                                  <Link
-                                    className="link"
-                                    to={`/tags/${kebabCase(tag)}/`}
-                                  >
-                                    # {tag}
-                                  </Link>
-                                </span>
-                              </li>
-                            ))}
-                          </ul>
-                        )}
-                        <small className="post-date">
-                          {post.frontmatter.date}
-                        </small>
+  return (
+    <Layout>
+      <section className='section'>
+        <div className='container content content-container'>
+          <Form />
+          <div className='columns'>
+            <div className='column'>
+              {posts.map(({ node: post }, index) => {
+                const isIndex0 = index === 0
+                const classNames = isIndex0 ? 'post-thumbnail is-index-0' : 'post-thumbnail'
+
+                const postThumbnail = () => (
+                  <img
+                    className={classNames}
+                    alt='post thumbnail'
+                    src={post.frontmatter.thumbnail.childImageSharp.fluid.src}
+                  />
+                )
+
+                return (
+                  <div key={post.id} className='post'>
+                    {!isTabletOrMobile && isIndex0 && postThumbnail()}
+                    <a href={post.fields.slug}>
+                      <h1 className='post-title'>{post.frontmatter.title}</h1>
+                    </a>
+                    {isTabletOrMobile && isIndex0 && postThumbnail()}
+                    <a href={post.fields.slug}>
+                      <div className='post-inner'>
+                        {!isIndex0 && postThumbnail()}
+                        <p className='post-excerpt'>{post.excerpt}</p>
                       </div>
+                    </a>
+                    <div className='post-footer'>
+                      {post.frontmatter.tags && (
+                        <ul className='post-tags'>
+                          {post.frontmatter.tags.map((tag) => (
+                            <li key={tag + `tag`}>
+                              <span className='tag is-primary post-tag'>
+                                <Link className='link' to={`/tags/${kebabCase(tag)}/`}>
+                                  # {tag}
+                                </Link>
+                              </span>
+                            </li>
+                          ))}
+                        </ul>
+                      )}
+                      <small className='post-date'>{post.frontmatter.date}</small>
                     </div>
-                  )
-                })}
-              </div>
-              <div className="column is-3">
-                <Sidebar />
-              </div>
+                  </div>
+                )
+              })}
+            </div>
+            <div className='column is-3'>
+              <Sidebar />
             </div>
           </div>
-        </section>
-      </Layout>
-    )
-  }
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
 IndexPage.propTypes = {
@@ -112,3 +112,5 @@ export const pageQuery = graphql`
     }
   }
 `
+
+export default IndexPage
